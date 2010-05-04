c709cb6437926c4080eb9a7576a3a84e63130fce
Florent Guilleux <florent2@gmail.com>
2010-05-04 15:22:22 -0500

    default command are now written as config commands

--- lib/iterm_rails.rb
+++ lib/iterm_rails.rb
@@ -23,30 +23,23 @@ require 'timeout'
 module ItermRails
 
   DEFAULT_CONFIG = {
-    :projects_dir           => Dir.getwd,
-    :launch_server          => true,
-    :launch_console         => true,
-    :launch_spork_for_rspec => false,
-    :launch_spork_for_cuc   => false, 
-    :rails3                 => false,
-    :prepend_command        => false, # example: 'rvm ruby-1.9.2-head'
+    :projects_dir     => Dir.getwd,
+    :prepend_command  => false, # example: 'rvm ruby-1.9.2-head'
+    :main             => "mate .; git st",
+    :server           => "script/server -p {{port:3000}}",
+    :console          => "script/console",    
+    :spork_rspec      => "spork -p {{port:8989}}",
+    :spork_cuc        => "spork cuc -p {{port:9989}}",    
   }
-
-  def create_tab(project_path, *commands)
-    project_name = project_path.basename
-    open_tab :new_tab do
-      write "cd #{project_path}"
-      set_title "- #{project_name}"
-    end
-  end
-
+  NON_COMMANDS = [:projects_dir, :prepend_command]
+  
   def find_available_port_from(port_number)
     result = port_number
     result += 1 while is_port_open?(result)
     result
   end
 
-  def is_a_rails_project_directory?(project_path)
+  def self.is_a_rails_project_directory?(project_path)
     %w{app config db script}.all? { |directory| project_path.join(directory).exist? }
   end
 
@@ -68,7 +61,7 @@ module ItermRails
     return false
   end
 
-  def merge_configs(config, custom_config_file_path)
+  def self.merge_configs(config, custom_config_file_path)
     if custom_config_file_path.exist?
       config.merge YAML::load(File.open custom_config_file_path) 
     else
@@ -89,40 +82,15 @@ module ItermRails
   puts "open new window with tabs for '#{project_path}'"
   puts " with config: #{config.inspect}"
 
-  Tab = Struct.new :title, :commands
-  tabs = []
-
-  if config[:launch_spork_for_rspec]
-    port = find_available_port_from 8989
-    tabs << Tab.new("spork #{port}", ["spork -p #{port}"])
-  end
-
-  if config[:launch_spork_for_cuc]
-    port = find_available_port_from 9989
-    tabs << Tab.new("spork cuc #{port}", ["spork cuc -p #{port}"])
-  end
-
-  if config[:launch_console]
-    command = if config[:rails3] then "rails console" else "script/console" end
-    tabs << Tab.new("console", [command]) 
-  end
-
-  tabs << Tab.new("main", ["mate ./ &"])
-
-  if config[:launch_console]
-    port = find_available_port_from 3000
-    command = if config[:rails3] then "rails server" else "script/server" end  
-    tabs << Tab.new("server #{port}", ["#{command} -p #{port}"])
-  end
-
   ItermWindow.open do
     project_name = project_path.basename
-    tabs.each do |tab|
+    commands = config.keys - NON_COMMANDS
+    commands.each do |command|
       open_tab :new_tab do
-        write "cd #{project_path}"
+        write "cd #{project_path}"        
         write config[:prepend_command] if config[:prepend_command]
-        tab.commands.each { |command| write command }
-        set_title "#{tab.title} - #{project_name}"
+        write config[command]        
+        set_title "#{command} - #{project_name}"
       end
     end
   end
